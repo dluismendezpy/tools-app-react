41609be15a82a07aedd56969d07e927a63747889
Luis Mendez <71355340+dluismendezpy@users.noreply.github.com>
2022-02-10 15:15:45 -0400

    Calculator

--- screens/Calculator.js
+++ screens/Calculator.js
@@ -1,12 +1,51 @@
-import React from "react";
-import { View, Button, Text } from "react-native";
+import React, { useState } from "react";
+import { View, TextInput, Text, StyleSheet, Button, Alert } from "react-native";
 
 const Calculator = () => {
+  let [n1, setN1] = useState(0);
+  let [n2, setN2] = useState(0);
+  let suma = n1 + n2;
+
   return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text>Calculator View</Text>
+    <View style={styles.container}>
+      <Text>Primer numero</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="num1"
+        onChangeText={(val) => setN1(parseInt(val))}
+      />
+
+      <Text>Segundo numero</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="num2"
+        onChangeText={(val) => setN2(parseInt(val))}
+      />
+
+      <View>
+        <Button
+          onPress={() => Alert.alert("Suma", `La suma es igual a ${suma}`)}
+          title="Sumar"
+        />
+      </View>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: "#777",
+    padding: 8,
+    margin: 10,
+    width: 200,
+  },
+});
 
 export default Calculator;
